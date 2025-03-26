8520fde353a7b84e6652645bd7fc14acc5f0f682
KOU050223 <kouhei20050223@icloud.com>
2025-03-26 17:23:58 +0900

    add Homeに部員作品

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,48 @@
-import { Avatar, For, VStack, Text, Center, Image } from "@chakra-ui/react";
+import { Container, Box, VStack, Image, SimpleGrid } from "@chakra-ui/react";
 import About from "@/pages/About";
 import News from "@/components/News";
 import Achievement from "@/components/Achievement";
+import ProjectCard from "@/components/ProjectCard";
+import SectionHeader from "@/components/SectionHeader";
 
 const Home = () => {
+  // Homeに載せる３作品のみ
+  const projects = [
+    {
+      title: "射的60s",
+      author: "Rita",
+      technologies: ["Unity", "C#"],
+      youtubeUrl: "https://youtu.be/V33WS5e4fNQ",
+      description:
+        "60秒間で的を撃ち、高得点を目指すゲームです。プレイヤーは制限時間内にできるだけ多くの的を撃ち、ハイスコアを目指します。的の種類によって得点が異なり、動く的はより高得点になっています。",
+      deployLink: "https://unityroom.com/games/shootinggallery60s",
+      githubLink: null,
+      articleLink: null,
+    },
+    {
+      title: "FIT-typing",
+      author: "Rita",
+      technologies: ["Unity", "C#", "Illustrator"],
+      youtubeUrl: "https://youtu.be/V33WS5e4fNQ",
+      description:
+        "タイピングゲームです。FITの授業や活動に関連した単語を素早くタイプして、スコアを競います。",
+      deployLink: "https://unityroom.com/games/fit-typing",
+      githubLink: null,
+      articleLink: null,
+    },
+    {
+      title: "リア充なんか蹴っ飛ばせ",
+      author: "Rita",
+      technologies: ["Unity", "C#"],
+      youtubeUrl: "https://youtu.be/V33WS5e4fNQ",
+      description:
+        "リア充キャラクターを蹴っ飛ばして距離を競うゲームです。パワーとアングルを調整して、できるだけ遠くまで飛ばしましょう。",
+      deployLink: "https://unityroom.com/games/riajuu_kickaway",
+      githubLink: null,
+      articleLink: null,
+    }
+  ];
+
   return (
     <VStack grap="18">
       <Image
@@ -18,6 +57,37 @@ const Home = () => {
       <About />
       <News />
       <Achievement />
+      {/* 部員作品紹介 */}
+      <Container maxW="75%" py={12} mx="auto">
+        <SectionHeader
+          size="3xl"
+          buttontext="他の作品を見る"
+          buttonhref="/projects"
+        >
+          部員作品紹介
+        </SectionHeader>
+        <Box my={10} textAlign="center">
+          <SimpleGrid
+            columns={{ base: 1, sm: 2, lg: 3 }}
+            spacing={10}
+            justifyItems="center"
+          >
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={index}
+                title={project.title}
+                author={project.author}
+                technologies={project.technologies}
+                youtubeUrl={project.youtubeUrl}
+                description={project.description}
+                deployLink={project.deployLink}
+                githubLink={project.githubLink}
+                articleLink={project.articleLink}
+              />
+            ))}
+          </SimpleGrid>
+        </Box>
+      </Container>
     </VStack>
   );
 };
